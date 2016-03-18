476bd8ea09e8df726414d2d01b18f91fca144df9
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-18 18:11:38 +0100

    implement first version of jmx memory

---
+++ src/test/java/de/benjaminborbe/jmx/memory/MainTest.java
@@ -0,0 +1,20 @@
+package de.benjaminborbe.jmx.memory;
+
+import java.io.IOException;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class MainTest {
+
+  @Test(expected = IOException.class)
+  public void testExecException() throws Exception {
+    Main.exec("localhost", 1234, "", "");
+  }
+
+  @Ignore
+  @Test
+  public void testExec() throws Exception {
+    System.out.println(Main.exec("192.168.99.100", 1099, "", ""));
+  }
+}
